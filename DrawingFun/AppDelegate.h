c5c6774f44d476bf0fcb5f32441415190388d8d5
renjwjx <renjwjx@gmail.com>
2016-11-24 17:06:29 +0800

    Initial Commit

---
+++ DrawingFun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DrawingFun
+//
+//  Created by jinren on 24/11/2016.
+//  Copyright © 2016 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DrawingFun/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DrawingFun
+//
+//  Created by jinren on 24/11/2016.
+//  Copyright © 2016 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
